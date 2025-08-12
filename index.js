6b70c2439f59730ea5c307946e816ef2e78aed4d
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-12 18:30:43 +0500

    Add Blackjack game logic to index.js for checking the logic
    Introduced conditional statements to check the sum of two cards and print appropriate messages for winning, continuing, or losing in a simple Blackjack game.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// 1. Create two variables, firstCard and secondCard.
 let firstCard = 7;
-
-// Set their values to a random number between 2-11
 let secondCard = 13;
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard;
 
 console.log(sum);
+
+if (sum === 21) {
+  console.log(`You Win 🥳. You've got Blackjack!`);
+} else if (sum < 21) {
+  console.log(
+    `You are still in the game 🙃. Do you want to draw a new card? 🙂`
+  );
+} else {
+  console.log(`You lost badly 🥴`);
+}
