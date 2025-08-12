f143eb5f618b4c5b05ffbd74751c30963ae6622e
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-12 18:40:25 +0500

    Changed Message and removed the extra console..log
    When I would run the game, It showed an extra value in console showing the sum. also the message had some irrelevant part.

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ let secondCard = 13;
 let sum = firstCard + secondCard;
 message = "";
 
-console.log(sum);
-
 if (sum === 21) {
   message = `You Win 🥳. You've got Blackjack!`;
 } else if (sum < 21) {
-  message = `You are still in the game 🙃. Do you want to draw a new card? 🙂`;
+  message = `You are still in the game 🙃.`;
 } else {
   message = `You lost badly 🥴`;
 }
