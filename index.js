42e1817dd0d84873809c03f33adfb45f0c727e9b
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-14 21:09:22 +0500

    Add START GAME button and refactor game logic
    Introduced a START GAME button in the UI and moved the game logic into a startGame() function, which is triggered by the button. Updated styles for the new button and adjusted container sizing for improved layout.

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ let secondCard = 13;
 let sum = firstCard + secondCard;
 message = "";
 
-if (sum === 21) {
-  message = `You Win 🥳. You've got Blackjack!`;
-} else if (sum < 21) {
-  message = `You are still in the game 🙃.`;
-} else {
-  message = `You lost badly 🥴`;
+function startGame() {
+  if (sum === 21) {
+    message = `You Win 🥳. You've got Blackjack!`;
+  } else if (sum < 21) {
+    message = `You are still in the game 🙃.`;
+  } else {
+    message = `You lost badly 🥴`;
+  }
+  console.log(message);
 }
-
-console.log(message);
