ab5da83b4d6bb74b20bc9e936ae7d6491310efd4
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-15 16:10:00 +0500

    Refactor card logic and add game state variables
    Introduces getRandomCard() to generate card values, refactors card and sum initialization, and adds isAlive and hasBlackJack state variables. Updates renderGame and newCard functions to use the new logic for improved game flow.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let firstCard = 7;
-let secondCard = 13;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
+let isAlive = false;
+let hasBlackJack = false;
+
 message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
@@ -11,7 +12,23 @@ function startGame() {
   renderGame();
 }
 
+function getRandomCard() {
+  const card = Math.floor(Math.random() * 13) + 1;
+  if (card === 1) {
+    return 11;
+  } else if (card > 10) {
+    return 10;
+  } else {
+    return card;
+  }
+}
+
 function renderGame() {
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  let cards = [firstCard, secondCard];
+  let sum = firstCard + secondCard;
+
   cardsEl.textContent = "Cards: ";
   for (let i = 0; i < cards.length; i++) {
     cardsEl.textContent += ` ${cards[i]}`;
@@ -20,16 +37,18 @@ function renderGame() {
   sumEl.textContent = `Sum: ${sum}`;
   if (sum === 21) {
     message = `You Win 🥳. You've got Blackjack!`;
+    hasBlackJack = true;
   } else if (sum < 21) {
     message = `You are still in the game 🙃.`;
   } else {
     message = `You lost badly 🥴`;
+    isAlive = false;
   }
   messageEl.textContent = message;
 }
 
 function newCard() {
-  let card = 7;
+  let card = getRandomCard();
   sum += card;
   cards.push(card);
 
